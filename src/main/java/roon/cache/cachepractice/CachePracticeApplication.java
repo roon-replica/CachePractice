922dca68014998bc93125b250326074a8a8bf778
roon-replica <think-roon@naver.com>
2023-03-27 17:52:30 +0900

    initial commit

---
+++ src/main/java/roon/cache/cachepractice/CachePracticeApplication.java
@@ -0,0 +1,13 @@
+package roon.cache.cachepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/roon/cache/cachepractice/CachePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package roon.cache.cachepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
